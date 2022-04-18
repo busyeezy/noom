228e6de87cee21860147cc3b0045abce2d0b562c
busyeezy <busyeezy@gmail.com>
2022-04-19 02:00:02 +0900

    Create chat system with WebSocket

--- src/server.js
+++ src/server.js
@@ -1,12 +1,43 @@
-import express from "express";
+import http from "http";
+import WebSocket from "ws";
+import express from "express"; // handling http
+import { parse } from "path";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  // socket => contact line with browser
+
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
+
+  console.log("Connected to the Browser");
+
+  socket.on("close", () => console.log("Disconnected from the Browser"));
+  socket.on("message", (message) => {
+    const parsed = JSON.parse(message);
+    switch (parsed.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${parsed.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = parsed.payload;
+        break;
+    }
+  });
+});
+
+server.listen(3000, () => console.log(`Listening on http://localhost:3000`));
